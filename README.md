58e1b8212b7ab8e18238c23cccffaf59c6e6d2cd
Daniel Ahmadpour <sagesheep@protonmail.com>
2017-11-17 03:19:45 +0100

    Added support for all official EU languages.

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Hello, Network!
-A simple python script that prints "Hello, Network!" to a terminal.
+> A simple python 3 script that prints "Hello, Network!" in 22 diffrent languages.
 
 ## Concept
 The basic idea is to change the metaphysical constant from "the world" (that speculative realist philosophers such as [Markus Gabriel](https://www.youtube.com/watch?v=_0vbXcRcHNA) points out doesn't exist). To the network, as an [intra-active](https://www.youtube.com/watch?v=v0SnstJoEec) phenomena.
@@ -9,11 +9,11 @@ To check if you have python on your machine open up a command line window and wr
 ```
 python -V
 ```
-If you have Python on your machine, the command should print the [Python version number](https://docs.python.org/3/using/cmdline.html#cmdoption-v) and exit. 
+If you have Python on your machine, the command should print the [Python version number](https://docs.python.org/3/using/cmdline.html#cmdoption-v) and exit.
 ### Prerequisites
-* [Python](https://wiki.python.org/moin/BeginnersGuide/Download)
+* [Python 3+](https://wiki.python.org/moin/BeginnersGuide/Download)
 
-### Installing 
+### Installing
 A straightforward guide how to install Python on your machine can find [here](https://wiki.python.org/moin/BeginnersGuide/Download).
 
 ## Running the code
@@ -21,9 +21,23 @@ To run the code open up a command line window and write:
 ```
 python hello.py
 ```
+Default language is set to English. You can also change the language by typing it in like this:
+
+```
+python hello.py spanish
+```
 ## Authors
 * **Daniel Ahmadpour** - *Initial work* - [sagesheep](https://github.com/sagesheep)
-Potential future collaborators can be found [here](https://github.com/sagesheep/hello-network/contributors).
+
+Future collaborators can be found [here](https://github.com/sagesheep/hello-network/contributors).
 
 ## License
 It's free to fork and do what you want with it, as the project it's under a  [kopimi](http://www.kopimi.com/) licence.
+
+## Contributing
+
+1. Fork it (<https://github.com/sagesheep/hello-network/fork>)
+2. Create your feature branch (`git checkout -b feature/fooBar`)
+3. Commit your changes (`git commit -am 'Add some fooBar'`)
+4. Push to the branch (`git push origin feature/fooBar`)
+5. Create a new Pull Request
--- hello.py
+++ hello.py
@@ -1,2 +1,66 @@
-# Print "Hello, Network!" to terminal
-print("Hello, Network!")
+# This python file uses the following encoding: utf-8
+import collections
+import sys
+
+# Creates an immutible (unchanging) data structure
+# It's great for concurrent programing supprted in Python 3
+Message = collections.namedtuple('Message', [
+    'language',
+    'message',
+])
+
+messages = (
+    Message(language='bulgarian',   message='Здравей, Мрежа!'),
+    Message(language='croatian',    message='Pozdrav, mreža!'),
+    Message(language='czech',       message='Dobrý den, síť!'),
+    Message(language='danish',      message='Hej, netværk!'),
+    Message(language='english',     message='Hello, Network!'),
+    Message(language='estonian',    message='Tere, võrk!'),
+    Message(language='finnish',     message='Hei, verkko!'),
+    Message(language='german',      message='Hallo, Netzwerk!'),
+    Message(language='greek',       message='Γεια σας, Δίκτυο!'),
+    Message(language='hungarian',   message='Helló, hálózat!'),
+    Message(language='irish',       message='Dia duit, Líonra!'),
+    Message(language='italian',     message='Ciao, rete!'),
+    Message(language='latvian',     message='Sveiki, tīkls!'),
+    Message(language='lithuanian',  message='Sveiki, Tinklas!'),
+    Message(language='maltese',     message='Bongu, Netwerk!'),
+    Message(language='polish',      message='Cześć, sieci!'),
+    Message(language='portuguese',  message='Olá, rede!'),
+    Message(language='romanian',    message='Bună, Rețea!'),
+    Message(language='slovak',      message='Dobrý deň, sieť!'),
+    Message(language='slovenian',   message='Pozdravljeni, mreženje!'),
+    Message(language='spanish',     message='Hola, ¡Red!'),
+    Message(language='swedish',     message='Hej, Nätverk!'),
+)
+
+
+# Check if language exist in messages, returns True or False
+def language_exist(language):
+    return any(x.language == language for x in messages)
+
+
+def language():
+    language = ' '.join(sys.argv[1:])
+    if not language_exist(language):
+        language = 'english'  # Default language
+    return language
+
+
+#  Filter messages after selected language
+def filter_language(language):
+    return [x for x in messages if x.language == language]
+
+
+def print_message(language):
+    message = ' '.join(
+        [str(x.message) for x in messages if x.language == language])
+    print(message)
+
+
+def main():
+    print_message(language())
+
+
+if __name__ == '__main__':
+    main()
